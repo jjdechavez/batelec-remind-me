b9f68f3e58702c3c3d6a64b0d864341ede5349fd
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-03-04 16:09:24 +0800

    Update fb scrape

--- fb-scrape.py
+++ fb-scrape.py
@@ -2,24 +2,30 @@ from facebook_scraper import get_posts
 import json
 import requests
 
-print("Starting FB scrape...")
+
+def get_posts():
+    file = open('batelec.json')
+    data = json.load(file)
+    return data
 
 
-file = open('batelec.json')
-data = json.load(file)
-
-posts = []
-
-for post in data:
-    posts.append(post)
-
-
-r = requests.post('http://localhost:3000/posts',
-                  data={'posts': json.dumps(posts)})
-print(r.json())
-
-# for post in get_posts('nintendo', page=1):
-#     posts.append(post)
+def send_posts(posts):
+    result = requests.post('http://localhost:3000/posts',
+                           data={'posts': json.dumps(posts)})
+    # with fbscrape
+    # for post in get_posts('nintendo', page=1):
+    #     posts.append(post)
+    # for post in posts:
+    #     print(post)
 
 
-print("Done FB scrape")
+def main():
+    # print("Starting FB scrape...")
+
+    posts = get_posts()
+    send_posts(posts)
+
+    # print("Done FB scrape")
+
+
+main()
